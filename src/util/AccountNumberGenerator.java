7b563d433fe4c23e337673a2658ad357907fffa9
samjh07 <samridhjha17@gmail.com>
2026-07-20 14:16:55 +0530

    Update AccountNumberGenerator.java

--- src/util/AccountNumberGenerator.java
+++ src/util/AccountNumberGenerator.java
@@ -1,5 +1,25 @@
 package util;
 
 public class AccountNumberGenerator {
-    
+
+    private static int currentNumber = 1000;
+
+    private AccountNumberGenerator() {
+        // Prevent object creation
+    }
+
+    public static String generateAccountNumber() {
+        currentNumber++;
+        return "ACC" + currentNumber;
+    }
+
+    public static void setCurrentNumber(int number) {
+        if (number > currentNumber) {
+            currentNumber = number;
+        }
+    }
+
+    public static int getCurrentNumber() {
+        return currentNumber;
+    }
 }
